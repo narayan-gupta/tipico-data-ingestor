583d9ebc48ff26741ac511bea2240212e71a5306
Narayan Gupta <narayangupta763@gmail.com>
2024-05-04 17:55:00 -0400

    Fix outcomes table

--- dbt/dim_outcomes.sql
+++ dbt/dim_outcomes.sql
@@ -1,12 +1,14 @@
 {{ config(materialized='table') }}
 
 SELECT
-    DISTINCT outcome.id AS outcome_id,
+    main.id as event_id,
+    market.id as market_id,
+    outcome.id as outcome_id,
     outcome.name,
     outcome.isTraded,
     outcome.trueOdds,
     outcome.status
-FROM
-    {{ source('tipico', 'sports-data') }},
-    UNNEST(markets) AS market,
+FROM    
+{{ source('tipico', 'sports-data') }} main, 
+    UNNEST(main.markets) AS market,
     UNNEST(market.outcomes) AS outcome
