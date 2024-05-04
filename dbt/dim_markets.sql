12bec4a284d40f899df681e357d161087466239c
Narayan Gupta <narayangupta763@gmail.com>
2024-05-04 17:54:32 -0400

    Fix markets table

--- dbt/dim_markets.sql
+++ dbt/dim_markets.sql
@@ -1,12 +1,12 @@
 {{ config(materialized='table') }}
 
 SELECT
-    DISTINCT market.id AS market_id,
+    main.id as event_id,
+    market.id AS market_id,
     market.name,
     market.type,
     market.status,
     market.mostBalancedLine,
     market.sgpEligable
-FROM
-    {{ source('tipico', 'sports-data') }},
-    UNNEST(markets) AS market
+FROM {{ source('tipico', 'sports-data') }} main,
+UNNEST(markets) AS market
